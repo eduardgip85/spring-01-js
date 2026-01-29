98e14e4e369ac685ec331222611573a2b258cea9
Eduard Goma <eduardgip85@gmail.com>
2026-01-29 11:23:18 +0100

    01 solved

--- exercises/01_block-scoping.test.js
+++ exercises/01_block-scoping.test.js
@@ -1,5 +1,7 @@
 const noop = () => {}
 test('01_scope-1: can be used in place of `var`', () => {
+  let bandName = 'Queen'
+  let isBestBand = true
   // Declara bandName utilitzant 'let'
   // Declara isBestBand utilitzant 'let'
 
@@ -9,20 +11,19 @@ test('01_scope-1: can be used in place of `var`', () => {
 
 test('01_scope-2: can modify the value of a `let` variable even in the next block statement', () => {
   // Declara releaseName utilitzant 'let'
-  var releaseName = 'ES6'
+  let releaseName = 'ES6'
   {
     // Modifica el valor de releaseName dins del bloc
     releaseName = 'ES2015'
   }
-  expect(releaseName).toBe(/* INTRODUEIX LA TEVA RESPOSTA AQUÍ */)
+  expect(releaseName).toBe("ES2015")
 })
 
 test('01_scope-3: cannot modify the value of a `const` variable', () => {
   function getReleaseName() {
     // Tria el teu costat. Li dius ES6 o ES2015?
     // No pots utilitzar `const` i reasignar el valor!
-    const releaseName = 'ES6' // Si li dius ES2015, canvia això a let o var
-    releaseName = 'ES2015' // Si li dius ES6, elimina aquesta reasignació
+    let releaseName = 'ES6' // Si li dius ES2015, canvia això a let o var
     return releaseName
   }
   expect(getReleaseName).not.toThrow()
@@ -31,7 +32,7 @@ test('01_scope-3: cannot modify the value of a `const` variable', () => {
 test('01_scope-4: is trapped inside of an `if` statement', () => {
   if (true) {
     // Canvia `var` a `let`, perquè b estigui limitat dins de l\'if
-    var b = 1
+    let b = 1
   }
   expect(() => noop(b)).toThrow('b is not defined')
 })
@@ -39,7 +40,7 @@ test('01_scope-4: is trapped inside of an `if` statement', () => {
 test('01_scope-5: can\'t redeclare using the same name', () => {
   function doLoop() {
     // Canvia el comptador del bucle a `let` perquè estigui limitat dins del bucle i no es pugui retornar.
-    for (var i = 0; i < 10; i++) {
+    for (let i = 0; i < 10; i++) {
       /* eslint no-empty:"off" */
     }
     return i
@@ -52,7 +53,7 @@ test('01_scope-6: means that we can start using block statements', () => {
   // BLOC DE CODI
   {
     // Canvia a una declaració `const`
-    var d = 2
+    const d = 2
   }
 
   expect(() => noop('d', d)).toThrow('d is not defined')
