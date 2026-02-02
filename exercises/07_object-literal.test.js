65b81365256f184753d9a7a25fb714865fa4bd67
Eduard Goma <eduardgip85@gmail.com>
2026-02-02 11:03:52 +0100

    07 solved

--- exercises/07_object-literal.test.js
+++ exercises/07_object-literal.test.js
@@ -1,14 +1,14 @@
 test('07_object-literal-1: can use shorthand for property names', () => {
   const  createMonster = (name, power) => {
     // Utilitza la nova sintaxi del literal d'Object, per tornar un literal que permeti passar els testos
-    // return {
-    //   type: 'Monster',
-    //   name: name,
-    //   power: power,
-    //   attack(target){
-    //     return `${this.name} attacked ${target.name}`;
-    //   }
-    // }
+    return {
+      type: 'Monster',
+      name,
+      power,
+      attack(target){
+        return `${this.name} attacked ${target.name}`;
+      }
+    }
   }
 
   const godzilla = createMonster('Godzilla', 1000)
@@ -25,6 +25,7 @@ test('07_object-literal-2: can use expressions as property names', () => {
       type,
       // afegeix una expressió com a nom de propietat on el nom de la propietat sigui el resultat de type.toUpperCase() + type.length
       // sembla forçat? Ho és... 😅
+      [type.toUpperCase() + type.length]: description
     }
   }
 
