9fc3a9360d8eba208ffb98c74acbdfa7bd4c0f9d
Eduard Goma <eduardgip85@gmail.com>
2026-02-02 10:33:09 +0100

    Solved 05

--- exercises/05_template-literals.test.js
+++ exercises/05_template-literals.test.js
@@ -11,7 +11,7 @@ test('05_template-literals-1: should support string interpolation', () => {
     ],
   }
   // construeix una cadena utilitzant la interpolació de literals de plantilla
-  const personsFriends = ``
+  const personsFriends = `${person.name} has 6 friends: ${person.friends[0]}, ${person.friends[1]}, ${person.friends[2]}, ${person.friends[3]}, ${person.friends[4]}, ${person.friends[5]}`
   expect(personsFriends).toBe(
     'Kent C. Dodds has 6 friends: Brooke Dodds, Matt Zabriskie, Aaron Frost, Dave Geddes, Joe Eames, Ryan Florence',
   )
@@ -19,23 +19,27 @@ test('05_template-literals-1: should support string interpolation', () => {
 
 test('05_template-literals-2: should support multi-line strings', () => {
   // construeix una cadena amb múltiples línies sense necessitat de caràcters de nova línia escapats
-  const multiLine = ``
+  const multiLine = `
+    How cool
+    is this!?
+  `
   expect(multiLine).toBe('\n    How cool\n    is this!?\n  ')
 })
 
 test('05_template-literals-3: should support string escaping', () => {
   // escapa correctament una cadena en un literal de plantilla per a cadascun d'aquests casos
-  expect(``).toBe('Hi\nthere!')
-  expect(``).toBe('This is `escaped` backticks')
+  expect(`Hi\nthere!`).toBe('Hi\nthere!')
+  expect(`This is \`escaped\` backticks`).toBe('This is `escaped` backticks')
 })
 
 //////// EXTRA CREDIT ////////
 
 // és probable que no utilitzis sovint l'etiquetatge, però pot ser útil!
-test.skip('05_template-literals-4: should call the tagging function', () => {
+test('05_template-literals-4: should call the tagging function', () => {
   const tagIt = (literalString, ...interpolatedParts) => {
-    // implementa aquesta funció per fer que el test passi
-    return 'fixme'
+    //literalstring = partes fijas
+    //interpolatedParts = valores dinamicos    
+    return (literalString[0] + 'super-cool ' + interpolatedParts[0] + literalString[1] + 'really ' + interpolatedParts[1] + literalString[2])
   }
   const noun = 'World'
   const emotion = 'happy'
