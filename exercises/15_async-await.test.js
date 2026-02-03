336ffafaec1ec2933a6f5cbb4e44bf0b309d78de
Eduard Goma <eduardgip85@gmail.com>
2026-02-03 11:42:44 +0100

    15 solved

--- exercises/15_async-await.test.js
+++ exercises/15_async-await.test.js
@@ -9,22 +9,32 @@ const doAsync = (rejectPromise = false) =>  new Promise((resolve, reject) => set
     })
   )
 
-  test('15_async-await-1: should work with resolved promises', async () => {
-  return doAsync().then(result => {
-    expect(result).toBe('resolved')
-    throw new Error(
-      'convert this to an async/await function and remove this error',
-    )
-  })
+test('15_async-await-1: should work with resolved promises', async () => {
+
+  const result = await doAsync()
+  expect(result).toBe('resolved')
+
+  // return doAsync().then(result => {
+  //   expect(result).toBe('resolved')
+  //   throw new Error(
+  //     'convert this to an async/await function and remove this error',
+  //   )
+  // })
 })
 
 test('15_async-await-2: should throw an error with a rejected promise', async () => {
-  return doAsync(true).catch(error => {
+  try {
+    await doAsync(true)
+  } catch (error) {
     expect(error).toBe('rejected')
-    throw new Error(
-      'convert this to an async/await function and remove this error',
-    )
-  })
+  }
+  
+  // return doAsync(true).catch(error => {
+  //   expect(error).toBe('rejected')
+  //   throw new Error(
+  //     'convert this to an async/await function and remove this error',
+  //   )
+  // })
 })
 
 
