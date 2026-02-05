ee3b48dff835305f1a86b03609df78275f49a112
Eduard Goma <eduardgip85@gmail.com>
2026-02-05 11:22:01 +0100

    25 solved

--- exercises/25_es2023.test.js
+++ exercises/25_es2023.test.js
@@ -2,8 +2,8 @@ test('25_es2023-1: Array findLast and findLastIndex', () => {
   //Utilitza els mètodes findLast i findLastIndex per trobar l'últim número parell i el seu índex.
   const array = [1, 2, 3, 4, 5]
 
-  const lastEven = null // Resol aquí...
-  const lastEvenIndex = null // Resol aquí...
+  const lastEven = array.findLast((element) => element == 4) // Resol aquí...
+  const lastEvenIndex = array.findLastIndex((element) => element == 4) // Resol aquí...
 
   expect(lastEven).toBe(4)
   expect(lastEvenIndex).toBe(3)
@@ -11,14 +11,14 @@ test('25_es2023-1: Array findLast and findLastIndex', () => {
 
 test('25_es2023-2: Hashbangs in JavaScript files', () => {
   //Escriu un script amb un hashbang i comprova que no causa errors.
-  const script = null // Resol aquí...
+  const script = `console.log('Hello')` // Resol aquí...
 
   expect(() => eval(script)).not.toThrow()
 })
 
 test('25_es2023-3: Symbol.prototype.description', () => {
   //Crea un símbol amb una descripció i comprova que la descripció és correcta.
-  const symbol = null // Resol aquí...
+  const symbol = Symbol('description') // Resol aquí...
 
   expect(symbol.description).toBe('description')
 })
